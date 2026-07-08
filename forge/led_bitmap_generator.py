c39363e35bd31f870b0f91a4bb746e4b53192a13
Taylor Local Agent <taylordangersheppard@gmail.com>
2026-07-08 10:24:24 -0700

    Add LED bitmap generator utility and test

--- forge/led_bitmap_generator.py
+++ forge/led_bitmap_generator.py
@@ -1,15 +1,16 @@
 #!/usr/bin/env python3
 
 import numpy as np
+from PIL import Image
 
-def generate_led_bitmap(width, height):
-    bitmap = np.zeros((height, width), dtype=int)
+def generate_led_bitmap(width=60, height=9):
+    bitmap = np.zeros((height, width), dtype=np.uint8)
     for y in range(height):
         for x in range(width):
             if (x + y) % 2 == 0:
-                bitmap[y, x] = 1
-    return bitmap
+                bitmap[y, x] = 255
+    return Image.fromarray(bitmap)
 
 if __name__ == '__main__':
-    bitmap = generate_led_bitmap(60, 9)
-    print(bitmap)
+    img = generate_led_bitmap()
+    img.save('led_bitmap.png')
